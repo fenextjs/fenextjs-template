296e4556d506b32c4fdec4e928af0d5a77946ffb
franciscoblancojn <blancofrancisco34@gmail.com>
2025-02-01 20:06:10 -0500

    add LayoutTable

--- src/pages/client/index.tsx
+++ src/pages/client/index.tsx
@@ -1,18 +1,30 @@
 import { useQueryClient } from '@/api/client/query';
 import { LayoutDashboard } from '@/layout/Dashboard';
+import { LayoutTable } from '@/layout/Table';
 import { TableClient } from '@/table/client';
+import { Text } from '@/ui-fenextjs/Text';
+import { Title } from '@/ui-fenextjs/Title';
 
 export const PageClient = () => {
     const { data, isLoading, error } = useQueryClient({});
     return (
         <>
             <LayoutDashboard>
-                <TableClient
-                    items={data?.data.items}
-                    nItems={data?.data.count}
-                    loader={isLoading}
-                    error={error?.error}
-                />
+                <LayoutTable
+                    top={<>
+                        <Title>Clientes</Title>
+                        <Text>
+                            Aqui puedes mirar tus clientes.
+                        </Text>
+                    </>}
+                >
+                    <TableClient
+                        items={data?.data.items}
+                        nItems={data?.data.count}
+                        loader={isLoading}
+                        error={error?.error}
+                    />
+                </LayoutTable>
             </LayoutDashboard>
         </>
     );
