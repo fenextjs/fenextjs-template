28071a7293562340a029aed32f9c6ccd0346f5cb
franciscoblancojn <blancofrancisco34@gmail.com>
2025-04-22 19:20:16 -0500

    add cors

--- src/api/endpoint/index.tsx
+++ src/api/endpoint/index.tsx
@@ -1,6 +1,27 @@
 import { ErrorFenextjs, IApiRespond } from 'fenextjs';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import Cors from 'cors'
 
+// Inicializar el middleware
+function initMiddleware(middleware: any) {
+    return (req: any, res: any) =>
+      new Promise((resolve, reject) => {
+        middleware(req, res, (result: any) => {
+          if (result instanceof Error) {
+            return reject(result);
+          }
+          return resolve(result);
+        });
+      });
+  }
+const cors = initMiddleware(
+    Cors({
+      methods: ['GET', 'POST', 'OPTIONS'],
+      origin: '*', // o un array con múltiples orígenes
+      credentials: true,
+    })
+  )
+  
 export const ApiEndPoint =
     <T,>(
         endpoint: (
@@ -10,6 +31,7 @@ export const ApiEndPoint =
     ) =>
     async (req: NextApiRequest, res: NextApiResponse<IApiRespond<T>>) => {
         try {
+            await cors(req, res)
             await endpoint(req, res);
         } catch (err: any) {
             const error: ErrorFenextjs = err;
