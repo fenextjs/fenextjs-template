24abfda0099c4f4a4319dc1869155d038fa5128b
franciscoblancojn <blancofrancisco34@gmail.com>
2025-04-22 19:26:41 -0500

    flb

--- src/api/endpoint/index.tsx
+++ src/api/endpoint/index.tsx
@@ -1,28 +1,28 @@
 import { ErrorFenextjs, IApiRespond } from 'fenextjs';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import Cors from 'cors'
+import Cors from 'cors';
 
 // Inicializar el middleware
 
 function initMiddleware(middleware: any) {
     return (req: any, res: any) =>
-      new Promise((resolve, reject) => {
-        middleware(req, res, (result: any) => {
-          if (result instanceof Error) {
-            return reject(result);
-          }
-          return resolve(result);
+        new Promise((resolve, reject) => {
+            middleware(req, res, (result: any) => {
+                if (result instanceof Error) {
+                    return reject(result);
+                }
+                return resolve(result);
+            });
         });
-      });
-  }
+}
 const cors = initMiddleware(
     Cors({
-      methods: ['GET', 'POST', 'OPTIONS'],
-      origin: '*', // o un array con múltiples orígenes
-      credentials: true,
-    })
-  )
-  
+        methods: ['GET', 'POST', 'OPTIONS'],
+        origin: '*', // o un array con múltiples orígenes
+        credentials: true,
+    }),
+);
+
 export const ApiEndPoint =
     <T,>(
         endpoint: (
@@ -32,7 +32,7 @@ export const ApiEndPoint =
     ) =>
     async (req: NextApiRequest, res: NextApiResponse<IApiRespond<T>>) => {
         try {
-            await cors(req, res)
+            await cors(req, res);
             await endpoint(req, res);
         } catch (err: any) {
             const error: ErrorFenextjs = err;
